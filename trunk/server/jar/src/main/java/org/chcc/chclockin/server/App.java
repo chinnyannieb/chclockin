37047173bc28963fb3d9af1432739510ead9616b
SamLJones <SamLJones@48e89b7e-a6ac-11dd-bf8d-3712e0689f7b>
2008-10-30 20:43:56 +0000

    Initial commit of the basic directory structure. still need to add groovy support, etc

---
+++ trunk/server/jar/src/main/java/org/chcc/chclockin/server/App.java
@@ -0,0 +1,13 @@
+package org.chcc.chclockin.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
